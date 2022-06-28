6b69ea277f50879f9551bbc40825b214ad0368dd
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-06-28 20:53:52 +0530

    completed first 2

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
 
+#declaring the app_name helps in refering to the specific url
+app_name = "encyclopedia"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>/", views.getPage, name="getPage"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,31 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
-
+#defining index function renders all the entries to the page
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+    if request == "POST":
+        print(request)
+    else:
+      return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()#returns the list of entires
     })
+
+
+def getPage(request, title):
+    page = util.get_entry(title)
+    #if the entry don't exists then renders the Not Found Page
+    if page is None: 
+        return render(request, "encyclopedia/page.html", {
+            "title": title.capitalize(),
+            "body": f"<h2 style='color: #86AAFF; margin-left: 40px; margin-top: 20px;'><i>{title}</i> not found</h2>"
+        })
+    #Else renders the page as an HTML file
+    else:
+        #print(page)
+        return render(request, "encyclopedia/page.html", {
+            "title": title.capitalize(),
+            "body": markdown2.markdown(page)
+        })
 
