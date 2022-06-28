46cdaa3eb33b96bd09ee12116abd3b692485d464
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-06-29 00:00:05 +0530

    completed till search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,16 +1,42 @@
+from turtle import title
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 import markdown2
 from . import util
 
-#defining index function renders all the entries to the page
+#defining index function 
 def index(request):
-    if request == "POST":
-        print(request)
+    #if accessed the POST request
+    if request.method == 'POST':
+        #accessing froms data and list of entries
+        title = request.POST.get('q')
+        res = util.get_entry(title)
+
+        #If item found then get render its page
+        if res != None:
+           return render(request, "encyclopedia/page.html", {
+            "title": title.capitalize(),
+            "body": markdown2.markdown(res)
+           })
+
+        #else if not then look through the list for substring and then render search.html
+        else:
+          entries = util.list_entries()
+          match_entries = []
+
+          for entry in entries:
+              if title.lower() in entry.lower():
+                  match_entries.append(entry)
+
+          return render(request, "encyclopedia/search.html", {
+            "title": title,
+            "entries": match_entries
+          })
+    #else if accessed via get then render all the entries
     else:
       return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()#returns the list of entires
-    })
+      })
 
 
 def getPage(request, title):
@@ -28,4 +54,5 @@ def getPage(request, title):
             "title": title.capitalize(),
             "body": markdown2.markdown(page)
         })
+
 
